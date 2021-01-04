be1016f299cd9587c56f6c131a1a8b331d2729d3
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-04 22:08:47 +0900

    5章完了

--- sample.js
+++ sample.js
@@ -1,15 +1,6 @@
-var color = "黒";
-switch(color) {
-    case "青":
-    case "緑":
-        console.log("進む");
-        break;
-    case "黃":
-        console.log("急いで進む");
-        break;
-    case "赤":
-        console.log("止まる");
-        break;
-    default:
-        console.log("信号機の故障");
-        }
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        continue;
+    }
+    console.log(i);
+}
