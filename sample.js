693a037e754367bb8b99e5081e64b993b4e548ec
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-05 16:51:08 +0900

    break文

--- sample.js
+++ sample.js
@@ -1,6 +1,8 @@
-for (var i = 1; i <= 10; i++) {
-    if (i % 2 == 0) {
-        continue;
+var tickets = [125, 177, 236, 395, 404, 492, 501, 580 ];
+for (var i = 0; i < tickets.length; i++) {
+    if (tickets[i] >= 500) {
+        break
     }
-    console.log(i);
+    console.log(tickets[i]);
 }
+console.log("終了");
