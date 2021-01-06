6d76f596c2459a2fbe9e4a7f89138e30da0c4ced
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-06 17:03:37 +0900

    関数

--- sample.js
+++ sample.js
@@ -1,8 +1,4 @@
-var user = {
-    name : "山田太郎",
-    age : 25,
-    heigth : 170
+let f = function () {
+    console.log("ABC");
 };
-for (var key in user) {
-    console.log(key + " : " + user[key]);
-}
+f();
