e12f20155eaaabb5687757d570c66d9ed11726d2
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-09 18:51:47 +0900

    9章完了

--- sample.js
+++ sample.js
@@ -1,3 +1,12 @@
-function check(e) {
-    console.log(e.value + "がクリックされました");
+function inputCheck() {
+    if(document.frm1.lastname.value == "") {
+        alert("性を入力してください");
+    } else if (document.frm1.firstname.value == "") {
+        alert("名を入力してください");
+    } else if (!document.frm1.gender[0] .checked &&
+        !document.frm1.gender[1] .checked) {
+            alert("性別を入力してください");
+        } else {
+            document.frm1.submit();
+        }
 }
