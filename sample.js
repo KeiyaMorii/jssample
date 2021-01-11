00e46be5ee7ed3359da93bd1c5a97940aa633c52
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-11 18:00:10 +0900

    7-1復習

--- sample.js
+++ sample.js
@@ -1,3 +1,13 @@
-(function (name) {
-    console.log(name + "さん、こんばんは");
-}) ("よしお");
+let obj1 = {
+    name: "よしお",
+    greet: function(age, from) {
+        console.log(this.name + "です、はじめまして");
+        console.log("年齢は" + age + "歳です");
+        console.log("出身地は" + from + "です");
+    }
+};
+let obj2 = {
+    name: "ユリ子"
+};
+let param = [6, "アメリカ"];
+obj1.greet.apply(obj2,param);
