1d844e07c78e26a16655b82bea3590647996dc16
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-11 18:41:51 +0900

    7章復習

--- sample.js
+++ sample.js
@@ -1,13 +1,5 @@
-let obj1 = {
-    name: "よしお",
-    greet: function(age, from) {
-        console.log(this.name + "です、はじめまして");
-        console.log("年齢は" + age + "歳です");
-        console.log("出身地は" + from + "です");
-    }
-};
-let obj2 = {
-    name: "ユリ子"
-};
-let param = [6, "アメリカ"];
-obj1.greet.apply(obj2,param);
+let today = new Date();
+console.log("今日は" + 
+    today.getFullYear() + "年" +
+    (today.getMonth() + 1) + "月" +
+    today.getDate() + "日です。");
