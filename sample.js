c001cdcf4a5cef45be4649e9983f6939cdc2ab62
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-14 21:23:15 +0900

    12-2完了

--- sample.js
+++ sample.js
@@ -1,7 +1,3 @@
-const olympic = new Date("2021-7-23");
-let hoge = new Date();
-let diff = olympic.getTime() - hoge.getTime();
-
-alert("オリンピックまでの日数は" + Math.floor(diff / 1000 / 60 / 60 / 24) + "日後です");
-
-console.log(new Date());
+$(function() {
+    $("p ~ a").css("font-size", "50px");
+});
