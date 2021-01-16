27e09b9cb8c7be57a26db33b103c0aabf615c8e6
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-16 18:27:58 +0900

    12-4完了

--- sample.js
+++ sample.js
@@ -1,3 +1,6 @@
 $(function() {
-    $("#msg").removeClass("large");
+    $("input[name='target']").on("change", function(){
+        $("#ippan_area, #hojin_area").toggleClass
+        ("active passive");
+    });
 });
