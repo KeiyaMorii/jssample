700fed5c17ab13d2e5efac7e0cbde6f53f607a73
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-16 17:33:04 +0900

    offメソッド

--- sample.js
+++ sample.js
@@ -1,11 +1,8 @@
 $(function() {
-    $("#btn")
-    .on("click", printHello)
-    .on("click", sayHello);
+    $("#btn").on("click", sayHello);
 });
-function printHello() {
-    console.log("こんにちは");
-}
 function sayHello() {
     alert("こんにちは");
+    $("#btn").off("click", sayHello);
+    $(this).attr("disabled", "disabled");
 }
