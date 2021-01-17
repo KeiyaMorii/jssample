3ea9a09d0aa5665d9b40efe5391ab60447efb8f3
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-17 13:17:59 +0900

    12章完了

--- sample.js
+++ sample.js
@@ -1,5 +1,20 @@
 $(function() {
-    $("#btn").on("click", function() {
-        $("p").replaceWith("<img src='images-j/frog2.jpg'><img>");
-    });
+    setSubCategory();
+    $("#main").on("change", setSubCategory);
 });
+function setSubCategory() {
+    let subList = [
+        [ "ノート", "タワー型", "一体型" ],
+        [ "マウス", "キーボード", "モニタ", "スピーカー"]
+    ];
+
+    let i = $("#main > option:selected").val();
+
+    let sub = $("#sub");
+    sub.empty();
+
+    for(let j=0; j < subList[i].length; j++) {
+        sub.append("<option value='" + j + "'>" + 
+        subList[i][j] + "</option>");
+    }
+}
