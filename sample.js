cb4f9ff519797729e241d7e2aa343a950757812b
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-17 16:04:28 +0900

    Ajax

--- sample.js
+++ sample.js
@@ -1,13 +1,3 @@
 $(function() {
-    $.getJSON("item.json",
-    function(data) {
-        for(let i=0; i<data.length; i++) {
-            $("#dataTable").append("<tr></tr>")
-            .find("tr:last")
-            .append("<td>" + data[i].code + "</td>")
-            .append("<td>" + data[i].name + "</td>")
-            .append("<td>" + data[i].price + "</td>");
-        }
-    }
-    );
+    $("#index2Area").load("index2.html");
 });
