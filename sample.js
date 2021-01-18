090c1baf3afa8557892a604bbb486a23e4ed24a9
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-18 11:45:49 +0900

    animateメソッドチェーン

--- sample.js
+++ sample.js
@@ -3,6 +3,9 @@ $(function() {
         $("#photo").animate({
             "width" : "90%",
             "height" : "90%"
-        }, 2000, "linear");
+        }, 3000, "swing").animate({
+            "width" : "20%",
+            "height" : "20%"
+        }, 300, "swing");
     });
 });
