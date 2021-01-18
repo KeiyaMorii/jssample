7c361dc736b44b8e962a6013f2f7fe69fcbcafb5
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-18 21:18:05 +0900

    到達度チェックQ6

--- sample.js
+++ sample.js
@@ -1,33 +1,8 @@
 $(function() {
-    let imageArea = $("#imageArea");
-    for(let i=1; i<=5; i++) {
-        imageArea.append(
-            "<img src='images-j/flower" + i + ".jpg'>"
-        );
-    }
-
-    $("#next").on("click", function() {
-        if(parseInt(imageArea.css("left")) > -800) {
-            toDisabled();
-            imageArea.animate({
-                "left" : "-=200px" 
-            }, "fast", "linear", toEnabled);
-        }
-    });
-
-    $("#prev").on("click", function() {
-        if(parseInt(imageArea.css("left")) < 0) {
-            toDisabled();
-            imageArea.animate({
-                "left" : "+=200px" 
-            }, "fast", "linear", toEnabled);
-        }
-    });
+    $("img").animate(
+        {"width" : "400px"},1000,"linear",toPassive
+    );
 });
-
-function toDisabled() {
-    $("#prev, #next").attr("disabled", "disabled");
-}
-function toEnabled() {
-    $("#prev, #next").removeAttr("disabled");
+function toPassive(){
+    $("img").hide();
 }
