b137ecaebdd91d4b501cded5fb639ac373fac0cf
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-20 21:13:17 +0900

    デバックコンソール

--- sample.js
+++ sample.js
@@ -1,9 +1,15 @@
 $(function() {
-    $("#btn").on("click", function() {
-        $(this).prop("disabled", true);
-    });
-
-    $("#btn2").on("click", function() {
-        $("#btn").prop("disabled", false);
-    });
+    $("#btn").on("click", showMessage);
 });
+function showMessage() {
+    let time = $("input[name='time']:checked").val();
+    if(time == "朝") {
+        alert("おはよう");
+    } else if(time == "昼") {
+        alert("こんにちは");
+    } else if(time == "夜") {
+        alert("こんばんは");
+    } else {
+        alert("いずれかの時間帯を選択して下さい");
+    }
+}
